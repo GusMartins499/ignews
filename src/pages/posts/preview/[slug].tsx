28030037af08a5fc072edbdc61519f2c7276bebc
gusmartins499 <gustavosm994@gmail.com>
2022-02-06 13:32:09 +0000

    finished app

--- src/pages/posts/preview/[slug].tsx
+++ src/pages/posts/preview/[slug].tsx
@@ -1,4 +1,4 @@
-import { GetStaticProps } from "next";
+import { GetStaticPaths, GetStaticProps } from "next";
 import { useSession } from "next-auth/react";
 import Head from "next/head";
 import Link from "next/link";
@@ -57,7 +57,7 @@ export default function PostPreview({ post }: IPostPreviewProps) {
   );
 }
 
-export const getStaticPaths = () => {
+export const getStaticPaths: GetStaticPaths = () => {
   return {
     paths: [],
     fallback: "blocking",
@@ -84,7 +84,8 @@ export const getStaticProps: GetStaticProps = async ({ params }) => {
 
   return {
     props: {
-      post
-    }
+      post,
+    },
+    redirect: 60 * 30, // 30 minutes
   }
 }
